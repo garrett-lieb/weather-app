4782f8591d571ba99d3ed92d98f88cddbb693d92
garrett lieb <liebg08@gmail.com>
2023-12-06 13:30:01 -0800

    started html and got api key from open weather, got geocode url to convert city and county names into geographical coordinates

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+/// 
+// weather api key 4111bc800396525093c9185f5d31c8cb
+
+// base url should look like: 
+// https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={4111bc800396525093c9185f5d31c8cb}
+
+//goecode url should look like:
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={4111bc800396525093c9185f5d31c8cb}
+
+// get lat and lon from geocode api
+// use lat and lon to get weather data from weather api
+// display weather data on page
